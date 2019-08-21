536dd0db52fcf0b5d3315a91e2e374f91aa73d47
Alexey_Lisenko <detoner777@gmail.com>
2019-08-21 06:33:34 +0300

    Header component

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class Main extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1><Header /></h1>
+			</div>
+		)
+	}
+}
+
+export default Main;
