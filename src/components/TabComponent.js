7c07139aab5a73091ae3da95cd032312fa0a9221
Alexey_Lisenko <detoner777@gmail.com>
2019-08-22 15:26:12 +0300

    Done with Tabs

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Tabs, Tab, TabList, } from 'react-tabs';
 import TabDoor from './tabs_nav/TabDoor';
 import TabDevices from './tabs_nav/TabDevices';
 import TabPrices from './tabs_nav/TabPrices';
@@ -7,15 +7,35 @@ import '../css/TabsNav.css'
 
 
 class TabComponent extends Component {
+	
+	state = {
+		tabIndex: 0
+	}
+
 	render() {
 		return(
-			<div>
-				<TabList>
-					<Tab><TabDoor /></Tab>	
-					<Tab><TabDevices /></Tab>	
-					<Tab><TabPrices /></Tab>	
+			<Tabs className="tabs" selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({tabIndex}) } >
+				<TabList className="tab-nav-container">
+					<Tab className={`${this.state.tabIndex === 0 ? 'tab-selected active' : null}`} >
+						<TabDoor />
+						<p style={{marginBottom: '1rem'}}><strong>No commitments <br />
+							Cancel oline at anytime</strong>
+						</p>
+					</Tab>	
+					<Tab className={`${this.state.tabIndex === 1 ? 'tab-selected active' : null}`}>
+						<TabDevices />
+						<p style={{marginTop: '-5.3125rem'}}>
+						<strong>Watch anywher</strong>
+						</p>
+					</Tab>	
+					<Tab className={`${this.state.tabIndex === 2 ? 'tab-selected active' : null}`}>
+						<TabPrices />
+						<p style={{marginTop: '0.2rem'}}>
+						<strong>Pick your price</strong>
+						</p>
+					</Tab>	
 				</TabList>
-			</div>
+			</Tabs>
 		)
 	}
 }
